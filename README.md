9839f4a1e6fec61a95e6b98f6bf0e3d99bb92a94
Malek Azadegan <azadeganmalek@gmail.com>
2021-01-04 20:26:26 -0500

    added identity matrix to constructor, added matrix subtraction, added exceptions, updated README.md

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ For learning python, linear algebra, and git all in one go!
 
 #### To do:
 - [x] Create matrix class
-    - [ ] Create identity matrix
+    - [x] Create identity matrix
     - [x] Randomize matrix entries
 - [x] Matrix addition
-- [ ] Matrix subtraction
+- [x] Matrix subtraction
 - [ ] Matrix multiplication
 - [ ] Determinant calculation
 - [ ] Minor calculation
@@ -20,15 +20,25 @@ For learning python, linear algebra, and git all in one go!
 #### Creating a matrix instance
 First parameter is rows, second parameter is columns. Third parameter is matrix type and is optional. Creates a zero matrix by default. There are two possible types:
 1. zero
-2. identity (todo)
+2. identity
 
 ```python
-matrix_instance = Matrix(3, 3, type = "zero")
+# Create a 3x3 zero matrix
+matrix = Matrix(3, 3, "zero")
+# OR
+matrix = Matrix(3, 3)
+
+# Create a 4x4 identity matrix
+identity_matrix = Matrix(4, 4, "identity")
+
+# Raises exception "Identity matrices must be square"
+exception_matrix = Matrix(3, 4, "identity")
+
 ```
 
 #### Matrix instance methods
 
-##### .show_self
+##### .show_self()
 Prints the matrix instance to console.
 
 ##### .randomize()
@@ -38,4 +48,7 @@ Sets every entry in the matrix instance to a random integer between 0 and 9.
 Returns a tuple with the amount of rows and columns in the matrix instance.
 
 ##### .add_matrix(second_matrix)
-Returns the sum of the matrix instance with second_matrix. Returns a new matrix instance.
+Returns the sum of the matrix instance with second_matrix. Returns a new matrix instance. Raises an exception if matrices are of different sizes.
+
+##### .subtract_matrix(second_matrix)
+Returns the difference of the matrix instance with second_matrix. Returns a new matrix instance. Raises an exception if matrices are of different sizes.
--- matrix.py
+++ matrix.py
@@ -2,7 +2,11 @@ import random
 
 class Matrix:
 
-    def __init__(self, rows = 1, columns = 1):
+    def __init__(self, rows = 1, columns = 1, type = "zero"):
+        if rows < 1 or columns < 1 :
+            raise Exception("Rows and columns must be greater than 0.")
+        if type == "identity" and rows != columns:
+            raise Exception("Identity matrices must be square.")
         # Set amount of rows and columns
         self.matrix = []
         self.rows = rows
@@ -10,10 +14,26 @@ class Matrix:
         # Create rows
         for i in range(rows):
             self.matrix.append([])
-        # Fill rows with 0s
+
+        # Makes zero matrix
         for i in self.matrix:
             for ii in range(columns):
                 i.append(0)
+
+        # Makes identity matrix
+        if type == "identity" and rows == columns:
+            for row in range(rows):
+                for col in range(columns):
+                    if row == col:
+                        self.matrix[row][col] = 1
+            else:
+                return None
+
+    # returns true if same size, false otherwise
+    def is_same_size(self, otherMatrix):
+        if self.rows == otherMatrix.rows and self.columns == otherMatrix.columns:
+            return True
+        return False
 
     def show_self(self):
         for row in self.matrix:
@@ -28,11 +48,14 @@ class Matrix:
                 self.matrix[row][entry] = random.randint(0, 9)
 
     def add_matrix(self, otherMatrix):
-        print "Adding"
+        # if matrices aren't the same size
+        if(self.is_same_size(otherMatrix) == False):
+            raise Exception("Matrices must be of the same size for addition.")
+
         self.show_self()
-        print "and"
+        print "+"
         otherMatrix.show_self()
-        print "Result = "
+        print "="
         resultMatrix = Matrix(self.rows, self.columns)
 
         for row in range(resultMatrix.rows):
@@ -41,13 +64,39 @@ class Matrix:
 
         resultMatrix.show_self()
 
+    def subtract_matrix(self, otherMatrix):
+        # if matrices aren't the same size
+        if(self.is_same_size(otherMatrix) == False):
+            raise Exception("Matrices must be of the same size for subtraction.")
+
+        self.show_self()
+        print "-"
+        otherMatrix.show_self()
+        print "="
+        resultMatrix = Matrix(self.rows, self.columns)
+
+        for row in range(resultMatrix.rows):
+            for col in range(resultMatrix.columns):
+                resultMatrix.matrix[row][col] = self.matrix[row][col] - otherMatrix.matrix[row][col]
+
+        resultMatrix.show_self()
+
+
 
 ### DO STUFF
 
-matrix1 = Matrix(3, 3)
-matrix1.randomize()
+# matrix1 = Matrix(3, 3)
+# matrix1.randomize()
+#
+# asdf = Matrix(3, 3)
+# asdf.randomize()
+#
+# matrix1.add_matrix(asdf)
 
-asdf = Matrix(3, 3)
-asdf.randomize()
+m1 = Matrix(2, 2)
+m1.randomize()
 
-matrix1.add_matrix(asdf)
+m2 = Matrix(3, 2)
+m2.randomize()
+
+m1.add_matrix(m2)
