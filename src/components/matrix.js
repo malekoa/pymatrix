382c3b554022e94de8915338c952c9b770bec907
Malek Azadegan <azadeganmalek@gmail.com>
2021-02-14 23:16:40 -0500

    cleared everything

--- src/components/matrix.js
+++
@@ -1,8 +0,0 @@
-class Matrix extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `<div>
-                            asdf
-                          </div>`
-    }
-}
-customElements.define('pymatrix-matrix', Matrix)
